8499054e94531667504aa0423395b526e3985f84
Cyqqut-nicnix-jodpo7 <thierry.dilenarda@gmail.com>
2022-09-09 18:08:17 +0200

    admin config for production

--- config/env/production/admin.js
+++ config/env/production/admin.js
@@ -2,6 +2,8 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
-  apiToken: null,
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
   url: '/dashboard',
 });
