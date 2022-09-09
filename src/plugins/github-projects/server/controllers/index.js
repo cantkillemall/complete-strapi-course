44130d0259884408a999e818cc685f61beef90b6
Cyqqut-nicnix-jodpo7 <thierry.dilenarda@gmail.com>
2022-09-09 17:12:40 +0200

    Extracted github projects plugin

--- src/plugins/github-projects/server/controllers/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const getReposController = require('./get-repos-controller');
-const projectController = require('./project-controller')
-
-module.exports = {
-  getReposController,
-  projectController
-};
--- src/plugins/github-projects/server/services/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const getReposService = require('./get-repos-service');
-const projectService = require('./project-service')
-
-module.exports = {
-  getReposService,
-  projectService
-};
