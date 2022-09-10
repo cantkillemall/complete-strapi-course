b7f79b3010b03163bd8420a23a925ef0809c9a1f
Cyqqut-nicnix-jodpo7 <thierry.dilenarda@gmail.com>
2022-09-10 10:56:24 +0200

    add cloudinary upload

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -19,5 +19,22 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {
+          folder: env("CLOUDINARY_FOLDER", 'udemy-strapi-test'),
+        },
+        delete: {},
+      },
+    },
+  },
   // ...
 });
